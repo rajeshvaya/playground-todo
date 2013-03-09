eb7e71aba2b829a70c155005aa215aeb0a5e894e
Rajesh Vaya <RV@varsha.local>
2013-03-09 21:22:35 +0400

    Added the functionality to remove an item from the todo list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,15 +1,18 @@
-
+//Todo controller
 function todoController($scope){
     
-    $scope.todos = [
-        {text: "Study angular js", done: false},
-        {text: "Build an todo app", done: false}
-    ];
+    //initialization
+    $scope.defaultTodo = {text: "You are awesome! you have no more todos to do :)", show: true};
+    $scope.todos = [];
     
+    //return total number of todos
     $scope.totalTodos = function(){
+        var todoLength = $scope.todos.length
+        $scope.defaultTodo.show = todoLength ? false : true
         return $scope.todos.length;
     }
     
+    //add a new todo in the list
     $scope.addTodo = function(){
         console.log($scope.addTodoText)
         if($scope.addTodoText == '' || $scope.addTodoText == undefined) return;
@@ -17,21 +20,29 @@ function todoController($scope){
         $scope.addTodoText = ''
     }
     
+    //edit a todo from  the list
+    $scope.editTodo = function(todo){
+        todo.editing = true;
+    }
+    
+    //remove a todo from the list
+    $scope.removeTodo = function(idx){
+        $scope.todos.splice(idx);
+    }
+    
+    //clear the completed todos (using underscore.js filter method)
     $scope.clearCompletedTodos = function(){
         $scope.todos = _.filter($scope.todos, function(todo){
             return !todo.done
         });
     }
     
-    $scope.editTodo = function(todo){
-        todo.editing = true;
-    }
-    
+    //reset flag after editing
     $scope.doneEditing = function(todo){
-        console.log("hiasd");
         todo.editing = false;
     }   
     
+    //empty method to do nothing
     $scope.doNothing = function(){
         return false;
     }
